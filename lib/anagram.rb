37260f4271c76c31bc52d97ffb4f1675df74f9a8
Ronald <rvpostha15@yahoo.com>
2023-01-30 19:50:51 -0500

    Tests Passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :name
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.select do |word|
+            word.downcase.chars.sort == @word.downcase.chars.sort 
+        end
+    end
+
+end
+
+
+
